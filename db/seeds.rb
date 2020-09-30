f165b0440dbb49e642cafb2b84a3ee80a6aa9b07
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-09-30 01:06:55 -0300

    Pre seed testing commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+file_path = Rails.root.join("public", "UF_2019.csv")
+    
+file = File.read(file_path.to_s)
+file = file.gsub(".", "").gsub(",", ".").gsub(";", ",")
+
+parsed_file = CSV.parse(file)
+parsed_file = parsed_file[1..parsed_file.length]
+
+for i in 0..parsed_file.length
+    parsed_file[i] = parsed_file[i].drop(1) unless parsed_file[i].nil?
+end
+
+for i in 0...parsed_file.length
+    for j in 0...parsed_file[i].length
+        Uf.create(month: j + 1, day: i + 1, value: parsed_file[i][j])
+    end
+end
