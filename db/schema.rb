2f22390322e7582ce38b6bfcd5543aed5127c989
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-10-13 22:44:18 -0300

    Generate model to save requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_235500) do
+ActiveRecord::Schema.define(version: 2020_10_14_013845) do
+
+  create_table "requests", force: :cascade do |t|
+    t.string "client"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ufs", force: :cascade do |t|
     t.integer "month", null: false
